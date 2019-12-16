2e5416bcea0ae23a673580d874186aa46bb03db9
Alexandr Sukhrin <alex.s@myualab.com>
2019-12-16 12:00:50 +0200

    [add] tests

--- src/test_api.py
+++
@@ -1,9 +0,0 @@
-from starlette.testclient import TestClient
-from api import app, startup
-
-
-async def test_tweets_view():
-    await startup()
-    client = TestClient(app=app)
-    res = client.get('/')
-    assert res.status_code == 200
---
+++ tests/test_api.py
@@ -0,0 +1,30 @@
+from starlette.testclient import TestClient
+from api import app, startup
+
+
+async def test_tweets_view():
+    await startup()
+    client = TestClient(app=app)
+    res = client.get('/')
+    assert res.status_code == 200
+
+
+async def test_amount_view():
+    await startup()
+    client = TestClient(app=app)
+    res = client.get('/amount')
+    assert res.status_code == 200
+
+
+async def test_hashtags_view():
+    await startup()
+    client = TestClient(app=app)
+    res = client.get('/hashtags')
+    assert res.status_code == 200
+
+
+async def test_users_view():
+    await startup()
+    client = TestClient(app=app)
+    res = client.get('/users')
+    assert res.status_code == 200
