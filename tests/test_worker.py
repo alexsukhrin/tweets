1d12657b16f5aa070eb4d971b60a8d1215f60c3a
Phoenix <alexandrvirtual@gmail.com>
2019-12-18 00:17:26 +0200

    [fix] test worker

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,13 +1,14 @@
 from prosapient.worker import Parser
 from unittest.mock import MagicMock
 import unittest
+from prosapient import config
 
 
 class TestParser(unittest.TestCase):
     def setUp(self) -> None:
         self.logger = MagicMock()
         self.engine = MagicMock()
-        self.conf = MagicMock()
+        self.conf = config
         self.test_parser = Parser(settings=self.conf, log=self.logger, conn_db=self.engine)
 
     def test_db_property(self):
